9fc0683e8e6dd493b4f121361850f9651eca023f
Khambrel Simpson <khamsimp18@gmail.com>
2024-12-10 23:45:38 -0800

    Changes!

--- blog/_config.ts
+++ blog/_config.ts
@@ -1,8 +1,14 @@
 import lume from "lume/mod.ts";
+import googleFonts from "lume/plugins/google_fonts.ts";
 
 const site = lume({
   src: "./src",
   dest: "./_site"
 });
+
+site.use(googleFonts({
+  fonts:
+    "https://fonts.google.com/share?selection.family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900",
+}));
 
 export default site;
