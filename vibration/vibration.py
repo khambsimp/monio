a80e6ead834a797ee2bb875183e974674379c523
Khambrel Simpson <khamsimp18@gmail.com>
2025-03-30 03:49:58 -0700

    New code.

--- vibration/vibration.py
+++ vibration/vibration.py
@@ -2,3 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(0, 0.25, 200)
+y = x + np.exp(x,3)
+
+fig, ax = plt.subplots()
+ax.set_title('VIB3705-3 Load vs. Deflection')
+ax.xlabel('Deflection (inches)')
+ax.ylabel('Load (lbf)')
+ax.plot(x, y)
+plt.savefig("vib3705-3_load_v_deflection.svg")
+plt.show()
