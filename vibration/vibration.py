e5eaf722f5c1f3cc72524b705eba29278f83e12e
Khambrel Simpson <khamsimp18@gmail.com>
2025-03-30 14:59:30 -0700

    load v. deflection graph created

--- vibration/vibration.py
+++ vibration/vibration.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(0, 0.25, 200)
-y = x + np.exp(x,3)
+y = x + np.exp(x,(3 * np.ones(200)))
 
 fig, ax = plt.subplots()
 ax.set_title('VIB3705-3 Load vs. Deflection')
-ax.xlabel('Deflection (inches)')
-ax.ylabel('Load (lbf)')
+ax.set_xlabel('Deflection (inches)')
+ax.set_ylabel('Load (lbf)')
 ax.plot(x, y)
 plt.savefig("vib3705-3_load_v_deflection.svg")
 plt.show()
