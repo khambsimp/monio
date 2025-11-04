c0b4c83959eaea0d5163542b17aa9a039ea6d561
Kham <khamsimp18@gmail.com>
2025-11-04 10:50:17 -0800

    Rename googleFonts import to fonts

--- blog/_config.ts
+++ blog/_config.ts
@@ -1,12 +1,12 @@
 import lume from "lume/mod.ts";
-import googleFonts from "lume/plugins/google_fonts.ts";
+import fonts from "lume/plugins/google_fonts.ts";
 
 const site = lume({
   src: "./src",
   dest: "./_site"
 });
 
-site.use(googleFonts({
+site.use(fonts({
   fonts:
     "https://fonts.google.com/share?selection.family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900",
 }));
